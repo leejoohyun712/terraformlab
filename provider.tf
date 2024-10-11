a6f151913db89baa15e6707434b2bde26776c2f6
terraform user <leejhhappy@naver.com>
2024-10-11 16:44:42 +0900

    파일추가됨

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.13.1"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "example" {
+    cidr_block = "172.17.0.0/16"
+
+    tags = {
+        Name = "terraform_vpc"
+    }
+}
